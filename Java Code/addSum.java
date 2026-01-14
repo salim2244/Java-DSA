f812d8e74dcca2adc34732838590f1bce95af575
salim2244 <salimansari8308@gmail.com>
2026-01-14 20:59:24 +0530

    New Files

---
+++ Java Code/addSum.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class addSum {
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int add = (a+b);
+        sc.close();
+        System.out.println(add);
+    }
+}
