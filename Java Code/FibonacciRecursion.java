77d8cc81537a2b81c71df278747d3e643ec7b86b
salim2244 <salimansari8308@gmail.com>
2026-01-20 12:31:30 +0530

    Febonaci using recursion

---
+++ Java Code/FibonacciRecursion.java
@@ -0,0 +1,16 @@
+public class FibonacciRecursion {
+    public static void main(String[] args) {
+        int n = 10;
+
+        for (int i = 0; i < n; i++) {
+            System.out.print(fib(i) + " ");
+        }
+    }
+
+    public static int fib(int n) {
+        if (n <= 1) {
+            return n;
+        }
+        return fib(n - 1) + fib(n - 2);
+    }
+}
